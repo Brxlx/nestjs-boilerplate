e147d43d1917e6f94d498d998cb830171940c37a
Bruno Santos <brcesar19@gmail.com>
2024-10-21 11:35:40 -0300

    chore: remove old eslint format to versiom >=9

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger: Logger = new Logger('App');
+
+  await app.listen(process.env.PORT ?? 3000, () => {
+    logger.log(`Application is running on port ${process.env.PORT ?? 3000}`);
+  });
 }
 bootstrap();
